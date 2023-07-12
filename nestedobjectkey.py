e361d694f4c98fe810f5c52c01ae60bcb96b2156
deepkumartraining <deep.training23@gmail.com>
2023-07-13 00:42:58 +0530

    comment updated for challenge #3

--- nestedobjectkey.py
+++ nestedobjectkey.py
@@ -1,4 +1,4 @@
-#Challenge #3
+#----------------Challenge #3--------------------------
 #We have a nested object. We would like a function where you pass in the object and a key and get back the value.
 #Example Inputs
 #object = {“a”:{“b”:{“c”:”d”}}}
