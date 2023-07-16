79a6b13d98aba4f837067439d71bfe4a2f196290
deepkumartraining <deep.training23@gmail.com>
2023-07-16 21:40:12 +0530

    updated for sg

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -3,269 +3,30 @@ resource "aws_security_group" "web-alb-sg" {
   vpc_id            = aws_vpc.vpc.id
   tags              = var.tags_sg_web
   description       = var.sg_description_web
-  ingress_rules_web = [
-  {
-    description     = "allow on 443"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
-    security_groups = null
-  },
-  {
-    description     = "allow on 80"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
-    security_groups = null
-  }
-]
-egress_rules_web = [
-  {
-    description     = "allow"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    security_groups = null
-  }
-]
-/*  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-*/  
-}
-resource "aws_security_group" "web_server-sg" {
-  vpc_id = aws_vpc.vpc.id
-  tags   = var.tags_sg_web_server
-  description = var.sg_description_web_server
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    security_groups = [aws_security_group.web-alb-sg.id]
-  }
-  egress_rules_web_server = [
-  {
-    description     = "allow"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    security_groups = null
-  }
-]
-
-
-
-/*  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-*/  
-}
-
-# App server - Security Group
-### This needs to configure for Web -> App -> DB layer details
-
-resource "aws_security_group" "app-alb-sg" {
-  vpc_id = aws_vpc.vpc.id
-  tags   = var.tags_sg_app
-  description = var.sg_description_app
-
-/*  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-*/
-ingress_rules_app = [
-  {
-    description     = "allow on 443"
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
-    security_groups = null
-  },
-  {
-    description     = "allow on 80"
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    cidr_blocks     = ["0.0.0.0/0"]
-    security_groups = null
-  }
-]
-egress_rules_app = [
-  {
-    description     = "allow"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    security_groups = null
-  }
-]  
-}
-
-resource "aws_security_group" "app_server-sg" {
-  vpc_id = aws_vpc.vpc.id
-  tags = var.tags_sg_web_server
-  description = var.sg_description_app_server
-  ingress_rules_app_server = [
-  {
-    description = "allow on 443"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = null
-    security_groups       = [aws_security_group.alb-sg.id]
-  },
-  {
     description = "allow on 80"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = null
-    security_groups       = [aws_security_group.alb-sg.id]
-  }
-]
-  egress_rules_app_server = [
-  {
-    description     = "allow"
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"]
     security_groups = null
   }
-]
 
-/*
   ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    security_groups = [aws_security_group.app-alb-sg.id]
+    description     = "allow on 443"
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    #security_groups = [aws_security_group.example_sg.id]
+    security_groups = null
   }
-
   egress {
+    description = "allow"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
-  }
-*/
-}
-
-# DB Security Group
-
-resource "aws_security_group" "db_sg" {
-  vpc_id = aws_vpc.vpc.id
-
-  ingress {
-    from_port   = 3306
-    to_port     = 3306
-    protocol    = "tcp"
-    security_groups = [aws_security_group.app_server-sg]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
+    security_groups = null
   }
 }
-
-
-
-
-
-
-
-
-
-/*resource "aws_security_group" "sg" {
-  name        = "${var.sg_name}"
-  description = "${var.sg_description}"
-  vpc_id      = var.vpc_id
-
-  dynamic "ingress" {
-    for_each = var.ingress_rules
-    content {
-      from_port   = ingress.value["from_port"]
-      to_port     = ingress.value["to_port"]
-      protocol    = ingress.value["protocol"]
-      cidr_blocks = ingress.value["cidr_blocks"]
-      description = ingress.value["description"]
-      security_groups = ingress.value["security_groups"]
-    }
-  }
-
-  dynamic "egress" {
-    for_each = var.egress_rules
-    content {
-      from_port   = egress.value["from_port"]
-      to_port     = egress.value["to_port"]
-      protocol    = egress.value["protocol"]
-      cidr_blocks = egress.value["cidr_blocks"]
-      description = egress.value["description"]
-      security_groups = egress.value["security_groups"]
-    }
-  }
-
-
-
-
-#   ingress {
-#     description      = "allow on 443"
-#     from_port        = 443
-#     to_port          = 443
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-
-#   ingress {
-#     description      = "allow on 80"
-#     from_port        = 80
-#     to_port          = 80
-#     protocol         = "tcp"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-
-#   egress {
-#     from_port        = 0
-#     to_port          = 0
-#     protocol         = "-1"
-#     cidr_blocks      = ["0.0.0.0/0"]
-#   }
-
-  tags = var.tags_sg
-}
-
-
-
-*/
