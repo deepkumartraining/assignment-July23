e7992fa1cbabaa5cabffc186ec421996b06a11c7
deepkumartraining <deep.training23@gmail.com>
2023-07-16 13:37:05 +0530

    TF issues fixed

--- terraform/nat.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_nat_gateway" "my_nat" {
-  allocation_id = aws_eip.eip_id
-  subnet_id     =  aws_subnet.my_public_subnet1.id
-  depends_on    = [aws_internet_gateway.gw]
-  tags = var.tags_nat
-  
-}
