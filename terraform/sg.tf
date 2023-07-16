9d9396c459e262326fb9443a3f610cdb8c535450
deepkumartraining <deep.training23@gmail.com>
2023-07-17 01:00:47 +0530

    resources updated

--- terraform/sg.tf
+++ terraform/sg.tf
@@ -105,19 +105,3 @@ resource "aws_security_group" "db_sg" {
     security_groups = [aws_security_group.ilb_intermediate_sg.id]
   }
 }
-
-# Create public subnets
-resource "aws_subnet" "public_subnet" {
-  count             = var.subnet_count
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.public_subnet_cidr_blocks[count.index]
-  availability_zone = element(var.availability_zones, count.index)
-}
-
-# Create private subnets
-resource "aws_subnet" "private_subnet" {
-  count             = var.subnet_count
-  vpc_id            = aws_vpc.vpc.id
-  cidr_block        = var.private_subnet_cidr_blocks[count.index]
-  availability_zone = element(var.availability_zones, count.index)
-}
--- terraform/sn.tf
+++ terraform/sn.tf
@@ -1,14 +1,15 @@
-# Create public and private subnets in multiple availability zones
+# Create public subnets
 resource "aws_subnet" "public_subnet" {
   count             = var.subnet_count
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(var.vpc_cidr_block, 8, count.index)
+  cidr_block        = var.public_subnet_cidr_blocks[count.index]
   availability_zone = element(var.availability_zones, count.index)
 }
 
+# Create private subnets
 resource "aws_subnet" "private_subnet" {
   count             = var.subnet_count
   vpc_id            = aws_vpc.vpc.id
-  cidr_block        = cidrsubnet(var.vpc_cidr_block, 8, count.index + 8)
+  cidr_block        = var.private_subnet_cidr_blocks[count.index]
   availability_zone = element(var.availability_zones, count.index)
 }
